ce4d27a846819b8bffcae8f552ab17ee4d73b276
bidemi oritunmise <bidoritunmise@gmail.com>
2019-08-12 16:40:10 -0700

    footer

--- src/components/fotter.jsx
+++ src/components/fotter.jsx
@@ -2,22 +2,115 @@ import React, { Component } from 'react';
 
 class Foot  extends Component {
     state = { 
-        homelogo:"image/whitelogo.png"
+        homelogo:"image/whitelogo.png",
+        secure:"image/secure.png",
+        rowspan:2
      }
     render() { 
         return ( 
             <React.Fragment>
-                <div id="footcontain" class="fnone">
-                    <div id="subscribe" class="fleft">
+                <div id="footcontain" className="fnone">
+                    <div id="subscribe" className="fleft">
                         <img alt="footlogo" src={this.state.homelogo} /><br/><br/>
-                        <span class="fevertext">Subscribe to our newsletter</span>
+                        <span className="fevertext">Subscribe to our newsletter</span>
                         <br/><br/>
-                        <input type="text" /> &nbsp;<button>subscribe</button>
+                        <input placeholder="Email Address" type="text" /> &nbsp;<button>SUBSCRIBE</button>
 			<br/><br/>
-			<span class="fevertext">&copy; 2019 All right reserved</span><br/>
-			<span>Secured by SSQL</span>
+			<span className="fevertext">&copy; 2019 All right reserved</span><br/>
+			<img alt="secure" src={this.state.secure} />
                     </div>
-		<div id="quicklink">
+		<div id="quicklink" className="fleft">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <span className="fevertext">Navigation</span>
+                        </td>
+                        <td>
+                            <span className="fevertext">Get Informed</span>
+                        </td>
+                        <td>
+                            <span className="fevertext">Discover</span>
+                        </td>
+                    </tr>
+                    <br/>
+                    <tr>
+                        <td>
+                            <span>
+                            Homepage
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                            FAQs
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                            Blog
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span>
+                            Get started
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                            Security
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                            About us
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span>
+                            Sign in
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                            Privacy policy
+                            </span>
+                        </td>
+                        <td>
+                            <span>
+                            Contact us
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span>
+                            How it works
+                            </span>
+                        </td>
+                        <td rowSpan = {this.state.rowspan}>
+                            <span>
+                            Terms of use
+                            </span>
+                        </td>
+                        <td rowSpan = {this.state.rowspan}>
+                            <span>
+                            <button>Get Started</button>
+                            </span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span>
+                            Formal complaints 
+                            </span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
 		</div>
 		
